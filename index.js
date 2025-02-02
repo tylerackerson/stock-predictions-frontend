bb1cffb0dddebcf162e4ec4b32f2aae714d39957
tyler ackerson <tyler.a.ackerson@gmail.com>
2025-02-02 08:36:58 -0800

    Update worker URLs

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ async function fetchStockData() {
     loadingArea.style.display = 'flex'
     try {
         const stockData = await Promise.all(tickersArr.map(async (ticker) => {
-            const url = `https://polygon-api-worker.guil-9d2.workers.dev/?ticker=${ticker}&startDate=${dates.startDate}&endDate=${dates.endDate}`
+            const url = `https://polygon-api-worker.tyler-a-ackerson.workers.dev/?ticker=${ticker}&startDate=${dates.startDate}&endDate=${dates.endDate}`
             const response = await fetch(url)
             if (!response.ok) {
                 const errMsg = await response.text()
@@ -82,7 +82,7 @@ async function fetchReport(data) {
     ]
     
     try {
-        const url = 'https://openai-api-worker.guil-9d2.workers.dev'
+        const url = 'https://openai-api-worker.tyler-a-ackerson.workers.dev'
         
         const response = await fetch(url, {
             method: 'POST',
